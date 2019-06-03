dab230208b14b8342a3442eab9d28c4df9e3c504
Rafael Gieschke <rafael.gieschke@rz.uni-freiburg.de>
2019-06-03 18:37:10 +0200

    Upgrade to Babel 7

--- src/babel-browser-build.js
+++ src/babel-browser-build.js
@@ -1,7 +1,7 @@
-window.babel = require('babel-core');
+window.babel = require('@babel/core');
 window.babelPlugins = [
-  require('babel-plugin-transform-es2015-modules-systemjs'),
-  require('babel-plugin-syntax-dynamic-import'),
-  require('babel-plugin-syntax-async-generators'),
-  require('babel-plugin-syntax-object-rest-spread'),
+  require('@babel/plugin-transform-modules-systemjs'),
+  require('@babel/plugin-syntax-dynamic-import'),
+  require('@babel/plugin-syntax-async-generators'),
+  require('@babel/plugin-syntax-object-rest-spread'),
 ];
